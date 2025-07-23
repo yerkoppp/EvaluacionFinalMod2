248896eca0f999ea4fa01087d94258ca650728c4
LUISFERGH1 <74872976+LUISFERGH1@users.noreply.github.com>
2025-07-23 18:37:02 -0400

    Update VisitaEnTerreno.java

--- src/prevencionDeRiesgos/VisitaEnTerreno.java
+++ src/prevencionDeRiesgos/VisitaEnTerreno.java
@@ -1,8 +1,8 @@
 package prevencionDeRiesgos;
-/**
- * @author Luis Guevara
- */
+
+
 public class VisitaEnTerreno {
+	//Atributos
 	private int identificadorVisitaEnTerreno;
     private int rutCliente;
     private String dia;
@@ -10,8 +10,20 @@ public class VisitaEnTerreno {
     private String lugar;
     private String comentarios;
 
+    /**
+	 * Constructor vacío.
+	 */
     public VisitaEnTerreno() {}
-
+    
+    /**
+     * Constructor
+     * @param identificadorVisitaEnTerreno
+     * @param rutCliente 
+     * @param dia 
+     * @param hora
+     * @param lugar
+     * @param comentarios
+     */
     public VisitaEnTerreno(int identificadorVisitaEnTerreno, int rutCliente, String dia,
                            String hora, String lugar, String comentarios) {
         this.identificadorVisitaEnTerreno = identificadorVisitaEnTerreno;
@@ -21,25 +33,109 @@ public class VisitaEnTerreno {
         this.lugar = lugar;
         this.comentarios = comentarios;
     }
+    
 
-    public int getIdentificadorVisitaEnTerreno() { return identificadorVisitaEnTerreno; }
-    public void setIdentificadorVisitaEnTerreno(int id) { this.identificadorVisitaEnTerreno = id; }
+  //Métodos
+    /**
+     * Obtiene el ID de la visita.
+     * @return ID de la visita.
+     */
+    public int getIdentificadorVisitaEnTerreno() {
+        return identificadorVisitaEnTerreno;
+    }
 
-    public int getRutCliente() { return rutCliente; }
-    public void setRutCliente(int rutCliente) { this.rutCliente = rutCliente; }
+    /**
+     * Asigna el ID de la visita.
+     * @param id Nuevo ID.
+     */
+    public void setIdentificadorVisitaEnTerreno(int id) {
+        this.identificadorVisitaEnTerreno = id;
+    }
 
-    public String getDia() { return dia; }
-    public void setDia(String dia) { this.dia = dia; }
+    /**
+     * Obtiene el RUT del cliente.
+     * @return RUT del cliente.
+     */
+    public int getRutCliente() {
+        return rutCliente;
+    }
 
-    public String getHora() { return hora; }
-    public void setHora(String hora) { this.hora = hora; }
+    /**
+     * Asigna el RUT del cliente.
+     * @param rutCliente Nuevo RUT.
+     */
+    public void setRutCliente(int rutCliente) {
+        this.rutCliente = rutCliente;
+    }
 
-    public String getLugar() { return lugar; }
-    public void setLugar(String lugar) { this.lugar = lugar; }
+    /**
+     * Obtiene el día de la visita.
+     * @return Día.
+     */
+    public String getDia() {
+        return dia;
+    }
 
-    public String getComentarios() { return comentarios; }
-    public void setComentarios(String comentarios) { this.comentarios = comentarios; }
+    /**
+     * Asigna el día de la visita.
+     * @param dia Nuevo día.
+     */
+    public void setDia(String dia) {
+        this.dia = dia;
+    }
 
+    /**
+     * Obtiene la hora de la visita.
+     * @return Hora.
+     */
+    public String getHora() {
+        return hora;
+    }
+
+    /**
+     * Asigna la hora de la visita.
+     * @param hora Nueva hora.
+     */
+    public void setHora(String hora) {
+        this.hora = hora;
+    }
+
+    /**
+     * Obtiene el lugar de la visita.
+     * @return Lugar.
+     */
+    public String getLugar() {
+        return lugar;
+    }
+
+    /**
+     * Asigna el lugar de la visita.
+     * @param lugar Nuevo lugar.
+     */
+    public void setLugar(String lugar) {
+        this.lugar = lugar;
+    }
+
+    /**
+     * Obtiene los comentarios de la visita.
+     * @return Comentarios.
+     */
+    public String getComentarios() {
+        return comentarios;
+    }
+
+    /**
+     * Asigna los comentarios de la visita.
+     * @param comentarios Nuevos comentarios.
+     */
+    public void setComentarios(String comentarios) {
+        this.comentarios = comentarios;
+    }
+
+    /**
+     * Retorna una descripción completa de la visita.
+     * @return Descripción de la visita.
+     */
     @Override
     public String toString() {
         return "Visita en Terreno #" + identificadorVisitaEnTerreno + "\n" +
@@ -49,6 +145,8 @@ public class VisitaEnTerreno {
                "Lugar: " + lugar + "\n" +
                "Comentarios: " + comentarios;
     }
+}
+
 }
 
 
